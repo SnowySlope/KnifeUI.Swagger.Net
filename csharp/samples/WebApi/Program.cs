938e55746b8e867e9105fd59ce3a99df3406a2e8
SnowySlope <rivera_dana2619@yahoo.com>
2023-05-24 11:43:29 +0800

    test

--- csharp/samples/WebApi/Program.cs
+++ csharp/samples/WebApi/Program.cs
@@ -25,7 +25,6 @@ builder.Services.AddControllers()
         options.SuppressModelStateInvalidFilter = true;
         options.SuppressMapClientErrors = true;
         options.ClientErrorMapping[404].Link = "https://*/404";
-
     });
 builder.Services.AddHttpContextAccessor();
 //配置全局获取HttpContext
